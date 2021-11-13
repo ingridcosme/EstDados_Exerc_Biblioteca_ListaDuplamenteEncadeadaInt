321bbb80f800994d0a3f253799383acd5fde6c81
ingri <ingri@LAPTOP-T6167IEQ>
2021-11-12 21:05:47 -0300

    Primeira versão

---
+++ src/br/edu/fateczl/ingridcosme/listaencadeadaduplaint/No.java
@@ -0,0 +1,12 @@
+package br.edu.fateczl.ingridcosme.listaencadeadaduplaint;
+
+public class No {
+	int dado;
+	No proximo;
+	No anterior;
+	
+	@Override
+	public String toString() {
+		return String.valueOf(dado);
+	}
+}
